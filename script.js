769c97a14b2265879300afe3693f8f1c1a6a3696
mrmystrie2022-maker <mrmystrie2022@gmail.com>
2026-05-03 22:29:52 +0530

    Add GSAP animations for hero section and particles

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ document.getElementById("intro").addEventListener("click", () => {
   });
 });
 
+gsap.from(".hero h1", {
+  opacity: 0,
+  y: 50,
+  duration: 1.2
+});
+
+gsap.from(".hero p", {
+  opacity: 0,
+  y: 30,
+  delay: 0.3,
+  duration: 1
+});
+
 // Scroll animations
 gsap.utils.toArray("section").forEach((sec) => {
   gsap.from(sec, {
@@ -25,3 +38,12 @@ gsap.utils.toArray("section").forEach((sec) => {
     duration: 1
   });
 });
+const particleContainer = document.querySelector(".particles");
+
+for (let i = 0; i < 25; i++) {
+  let span = document.createElement("span");
+  span.style.left = Math.random() * 100 + "vw";
+  span.style.animationDuration = (5 + Math.random() * 10) + "s";
+  span.style.width = span.style.height = (4 + Math.random() * 6) + "px";
+  particleContainer.appendChild(span);
+}
